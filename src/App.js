925824bf3a109afba97a5c78ffb92d68e013984b
slngvitoria@gmail.com <slngvitoria@gmail.com>
2022-03-06 16:08:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const StyleApp = styled.div`
+  display: flex;
+  height: 100vh;
+  align-items:center;
+  flex-direction:column;
+  justify-content:center;
+  background-color:${props=>props.color};
+`
 
 function App() {
+
+  const [title, setTitle] = useState('titulo inicial');
+  const [showContent, setShowContent] = useState(false);
+  const [color, setColor] = useState('white');
+
+  function handleTitle () {
+    (title === 'titulo inicial' ? setTitle('titulo modificado') : setTitle('titulo inicial'))
+  }
+
+  function handleContent () {
+    setShowContent(!showContent);
+  }
+
+  function ExclusiveContent () {
+    return (
+      <h2> No ceú tem pão? </h2>
+    )
+  }
+
+  function handleColor () {
+    setColor(gerarCor());
+  }
+
+  function gerarCor(opacidade = 1) {
+    let r = Math.random() * 255;
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
+    
+    return `rgba(${r}, ${g}, ${b}, ${opacidade})`;
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyleApp color={color}>
+      <h1>{title}</h1>
+      <button onClick={handleTitle}>Alterar o titulo</button>
+      &nbsp;
+      {showContent && <ExclusiveContent />}
+      <button onClick={handleContent}>{showContent ? 'Clique para esconder o conteudo': 'Clique para exibir o conteudo'}</button>
+      &nbsp;
+      <button onClick={handleColor}>Alterar a cor</button>
+    </StyleApp>
   );
 }
 
